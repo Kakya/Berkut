286e2ec5921bd3af6146f778fd56508fb180f171
Kakya <husseinzamzamy14@gmail.com>
2015-04-22 18:42:14 -0400

    Version

--- js/main.js
+++ js/main.js
@@ -218,6 +218,6 @@ function render() {
 	game.debug.body(card);
 	game.debug.text('Afterburner on for: '+ctr, 32, 92);
 	game.debug.text('Controls: W: add thrust. D,S: Roll control. Shift+W: Afterburner. Left mouse click: Fire missile.', 112,32);
-	game.debug.text('Version 26', 32, 112);
+	game.debug.text('Version 27', 32, 112);
 }
 };
