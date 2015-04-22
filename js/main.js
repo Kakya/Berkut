ef33be28229b9e0dfb55253f7ea934b18910149f
Kakya <husseinzamzamy14@gmail.com>
2015-04-21 20:52:15 -0400

    Bug Fix #2

--- js/main.js
+++ js/main.js
@@ -47,7 +47,7 @@ function create() {
     card = game.add.sprite(200, 200, 'card');
 	card.enableBody = true;
 	//card.body.clearShapes();
-	card.body.setsize(20,20);
+	card.body.setSize(20,20);
 	eagles = game.add.group(); 
 	eagles.enableBody = true;
     game.camera.follow(card);
